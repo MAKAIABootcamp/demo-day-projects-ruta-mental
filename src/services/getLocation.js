8d154597228b9ac41d6d747af820277add77a55a
Gustavo Silva <silvanaranjogustavoandres@gmail.com>
2022-12-11 19:50:57 -0500

    get user location update

--- src/services/getLocation.js
+++ src/services/getLocation.js
@@ -1,10 +1,12 @@
 import axios from "axios"
-const API_URL = 'https://ipinfo.io/json?token=f53f388a4598a2';
-export const getUserLocation = async() => {
-    try {
-        let { data } = await axios.get(API_URL);
-        console.log(data)
-    } catch (error) {
-        console.log(error)
+const apiKey = 'AIzaSyD6PZyuQRcFcGpMQNZptnHLaE31CaIEkTM'
+
+export const getUserLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition( async(position) => {
+            console.log(position)
+            let response = await axios.get("https://maps.googleapis.com/maps/api/geocode/json?latlng="+ position.coords.latitude + "," + position.coords.longitude +"&sensor=false"+"&key="+apiKey)
+            console.log(response)
+        })
     }
 }
