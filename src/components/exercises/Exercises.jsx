2623fca8c85a870e4a6f50eb0d17938e7df3425e
Jennifer Parra <jspn48@gmail.com>
2022-12-11 21:49:50 -0500

    Some minor changes

--- src/components/exercises/Exercises.jsx
+++ src/components/exercises/Exercises.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import NavBar from '../navbar/Navbar'
 import './exercises.scss'
 
+
 const Exercises = () => {
+
   return (
     <div className='body'>
-        <NavBar />
-        <header className='headerNav'>
-            <h1>Exercises</h1>
-        </header>
+      <NavBar />
+      <header className='headerNav'>
+        <h1>Exercises</h1>
+      </header>
     </div>
   )
 }
